f7576c0002396543b93a5ff258a302ce7f6250bb
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2025-01-23 11:19:26 +0100

    feat(ui): add shadcn-ui button

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,7 +1,10 @@
+import { Button } from '~/components/ui/button';
+
 export function App() {
   return (
-    <div className="h-screen w-full flex items-center text-center justify-center">
+    <div className="h-screen w-full flex items-center text-center justify-center flex-col">
       This is app.
+      <Button>Hello</Button>
     </div>
   );
 }
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import path from 'path';
 
 export default defineConfig({
   root: __dirname,
@@ -21,6 +22,11 @@ export default defineConfig({
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
+    },
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
     },
   },
 });
